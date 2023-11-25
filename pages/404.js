c2b4e581158fab9a8fc6e5f9e2acd9af0231173e
snoopy1224 <misty12024@gmail.com>
2023-11-25 21:46:09 +0900

    update:404

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,11 @@
+import ErrorAnimation from "@/components/home/error-animation";
 import Layout from "@/components/layout";
 
 export default function PageNotFound() {
   return (
     <Layout>
-      <h1>페이지를 찾을 수 없어요.</h1>
+      <h1 className="text-center title-font p-5 sm:text-4xl text-3xl mb-4 font-medium text-gray-900"></h1>
+      <ErrorAnimation />
     </Layout>
   );
 }
