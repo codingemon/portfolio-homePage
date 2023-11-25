3f9ab6a9633839eff7e7db17226c428ebe7e9025
snoopy1224 <misty12024@gmail.com>
2023-11-25 21:48:20 +0900

    fix:404

--- components/home/error-animation.js
+++ components/home/error-animation.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Lottie from "react-lottie-player";
 
-import lottieJson from "@/public/404Error2";
+import lottieJson from "@/public/404error2";
 
 export default function ErrorAnimation() {
   return <Lottie loop animationData={lottieJson} play />;
